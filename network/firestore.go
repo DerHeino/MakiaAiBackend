d18fc3b0915e393745f5071251c2e2dc1f021a4a
DerHeino <57097124+DerHeino@users.noreply.github.com>
2022-06-14 00:18:51 +0200

    another test

--- network/firestore.go
+++ network/firestore.go
@@ -27,7 +27,7 @@ var DeviceList map[string]model.LocalDevice
 func Start_firebase() *firestore.Client {
 
 	d, _ := json.Marshal(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-	fmt.Println(d)
+	fmt.Println(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 	// Use a service account
 	ctx := context.Background()
 	sa := option.WithCredentialsJSON(d)
